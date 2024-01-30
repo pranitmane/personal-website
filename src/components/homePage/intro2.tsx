9aee9b7c092e0e1448c8a5fc288b2f3f6727c1d2
pranit mane <pranitmane@pranits-MacBook-Pro.local>
2024-01-30 19:22:50 +0530

    added icon

--- src/components/homePage/intro2.tsx
+++ src/components/homePage/intro2.tsx
@@ -3,7 +3,7 @@ export default function IntroCard2(){
         <section className="w-full h-60 flex flex-col justify-center" >
             <h1 className="text-xl">Hey, I&apos;m Pranit,</h1>
             <h2 className="text-3xl font-bold">I build <span className="text-green-300">Full-stack</span> apps</h2>
-            <p>currently I&apos;m in prefinal year of Computer Engineering. I love learning about new technologies and sharing my insights through my blogs.</p>
+            <p className="text-gray-300" >currently I&apos;m in prefinal year of Computer Engineering. I love learning about new technologies and sharing my insights through my blogs.</p>
         </section>
     )
 }
